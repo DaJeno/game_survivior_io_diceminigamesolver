61176dadce9d678d465470f39ec29393564a99fc
Drake Jeno <drake.jeno@outlook.com>
2024-02-11 12:53:38 -0600

    Test remote push and some cleanup

--- STS.py
+++ STS.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+
+from IPython.Debugger import Tracer; debug_here = Tracer()
+
 board = [[0 for j in range(7)] for i in range(9)]
 s_dict = {
     "r": 25,
@@ -9,12 +13,32 @@ r = 25
 g = 5
 p = 10
 b = 0
-start = tuple([4,3])
-f = open("board.txt", "r")
-for i in range(9):
-    row = f.readline()[0:7]
-    for j in range(7):
-        board[i][j] = row[j]
+
+# Starting position is always the same
+START = tuple([4,3])
+
+def main():
+    # Read in the board
+    f = open("board.txt", "r")
+    for i in range(9):
+        row = f.readline()[0:7]
+        for j in range(7):
+            board[i][j] = row[j]
+
+    opt_path = dfs(15, [START], 0)
+    simplified = []
+    print(opt_path)
+    for step in range(1, len(opt_path[1])):
+        if opt_path[1][step][0] > opt_path[1][step-1][0]:
+            simplified.append("down")
+        elif opt_path[1][step][0] < opt_path[1][step-1][0]:
+            simplified.append("up")
+        elif opt_path[1][step][1] < opt_path[1][step-1][1]:
+            simplified.append("left")
+        else:
+            simplified.append("right")
+    print(simplified)
+
 def dfs(left, path, score):
     cur = path[len(path)-1]
     if cur[0] < 0 or cur[0] >= 9 or cur[1] < 0 or cur[1] >= 7:
@@ -33,20 +57,7 @@ def dfs(left, path, score):
         return None
     return max(results, key=lambda x: x[0])
 
-opt_path = dfs(15, [start], 0)
-simplified = []
-print(opt_path)
-for step in range(1, len(opt_path[1])):
-    if opt_path[1][step][0] > opt_path[1][step-1][0]:
-        simplified.append("down")
-    elif opt_path[1][step][0] < opt_path[1][step-1][0]:
-        simplified.append("up")
-    elif opt_path[1][step][1] < opt_path[1][step-1][1]:
-        simplified.append("left")
-    else:
-        simplified.append("right")
-print(simplified)
-
-
+if __name__ == "__main__":
+    main()
 
 
