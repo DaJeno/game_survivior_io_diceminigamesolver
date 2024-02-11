d1184500b8285bba3668e2543a74e185c9e84e38
Drake Jeno <drake.jeno@outlook.com>
2024-02-11 13:40:42 -0600

    Works and looks good enough

--- STS.py
+++ STS.py
@@ -1,21 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from IPython.Debugger import Tracer; debug_here = Tracer()
-
+# 2d array board
 board = [[0 for j in range(7)] for i in range(9)]
+
+# Value dict to convert color string to value
 s_dict = {
     "r": 25,
-    "g":5,
-    "p":10,
-    "b":0
+    "g": 5,
+    "p": 10,
+    "b": 0
 }
-r = 25
-g = 5
-p = 10
-b = 0
 
 # Starting position is always the same
-START = tuple([4,3])
+START = tuple([4, 3])
+
 
 def main():
     # Read in the board
@@ -26,38 +24,47 @@ def main():
             board[i][j] = row[j]
 
     opt_path = dfs(15, [START], 0)
-    simplified = []
+    simplified = ""
     print(opt_path)
     for step in range(1, len(opt_path[1])):
         if opt_path[1][step][0] > opt_path[1][step-1][0]:
-            simplified.append("down")
+            simplified += "D"
         elif opt_path[1][step][0] < opt_path[1][step-1][0]:
-            simplified.append("up")
+            simplified += "U"
         elif opt_path[1][step][1] < opt_path[1][step-1][1]:
-            simplified.append("left")
+            simplified += "L"
         else:
-            simplified.append("right")
+            simplified += "R"
     print(simplified)
 
+
 def dfs(left, path, score):
+    # Current position (row/col)
     cur = path[len(path)-1]
-    if cur[0] < 0 or cur[0] >= 9 or cur[1] < 0 or cur[1] >= 7:
+
+    # If the current position is out of bounds return None
+    if cur[0] not in range(0, 9) or cur[1] not in range(0, 7):
         return None
+
+    # Add the value of this position to the score total
     score += s_dict[board[cur[0]][cur[1]]]
     if left == 0:
         return [score, path]
-    next = [tuple([cur[0]+1, cur[1]]),tuple([cur[0]-1, cur[1]]),tuple([cur[0], cur[1]+1]),tuple([cur[0], cur[1]-1])]
+
+    # Create list of all possible next positions
+    next = [tuple([cur[0]+1, cur[1]]), tuple([cur[0]-1, cur[1]]),
+            tuple([cur[0], cur[1]+1]), tuple([cur[0], cur[1]-1])]
     results = []
     for coord in next:
         if coord not in path:
-            eval = dfs(left-1, path + [coord], score)
-            if eval != None:
-                results.append(eval)
+            result = dfs(left-1, path + [coord], score)
+            if result is not None:
+                results.append(result)
+
     if len(results) == 0:
         return None
     return max(results, key=lambda x: x[0])
 
+
 if __name__ == "__main__":
     main()
-
-
